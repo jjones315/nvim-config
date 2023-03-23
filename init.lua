6a9e20f599d0e06764b7d2432b5cea990591688a
Jayce Jones <jayce.jones@trinityonline.com>
2023-03-23 11:56:03 -0500

    first commit

---
+++ init.lua
@@ -0,0 +1,13 @@
+require('plugins')
+require('theme_vscode')
+require('lua_line')
+require('lsp_config')
+require('lspkind_config')
+require('mason_config')
+require('cmp_config')
+
+-- require'lspinstall'.setup()
+-- local servers = require'lspinstall'.installed_servers()
+-- for _, server in pairs(servers) do
+  -- require'lspconfig'[server].setup{}
+-- end
---
+++ lua/plugins.lua
@@ -0,0 +1,35 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+    -- packer itself...
+    use 'wbthomason/packer.nvim'
+
+    -- vscode theme
+    use 'Mofiqul/vscode.nvim'
+
+    -- lua line
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    }
+
+    -- lsp config
+    use {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig",
+    }
+
+    -- autocomplete
+    use {
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
+        "hrsh7th/nvim-cmp",
+        "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip",
+        "onsails/lspkind-nvim"
+    }
+
+end)
